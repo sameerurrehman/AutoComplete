c9137c3e27779993f18848b10c5897c9ab597f54
Ijara Developer5 <ijaradeveloper4@outlook.com>
2022-08-01 12:08:09 +0500

    AutoComplete work

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+import {ServService} from '../app/serv.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'autocomplete';
+
+  options = ["Sam", "Varun", "Jasmine"];
+
+  filteredOptions;
+
+
+  formGroup : FormGroup;
+  constructor(private service : ServService, private fb : FormBuilder){}
+
+  ngOnInit(){
+    this.initForm();
+    this.getNames();
+  }
+
+  initForm(){
+    this.formGroup = this.fb.group({
+      'employee' : ['']
+    })
+    this.formGroup.get('employee').valueChanges.pipe(debounceTime(1000)).subscribe(response => {
+      console.log('Entered data is ', response);
+      if(response && response.length){
+        this.filterData(response);
+      }else{
+        this.filteredOptions = [];
+      }
+      //this.filterData(response);
+    })
+  }
+
+  filterData(enteredData){
+    this.filteredOptions = this.options.filter(item => {
+      return item.toLowerCase().indexOf(enteredData.toLowerCase()) > -1
+    })
+  }
+
+  getNames(){
+    this.service.getData().subscribe(response => {
+      this.options = response;
+      //this.filteredOptions = response;
+    })
+  }
+
+
+  
+}
